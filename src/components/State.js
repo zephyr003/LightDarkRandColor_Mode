cbe9845211835b1eb8958f5cf65d2b169c9e9362
zephyr003 <115259876+zephyr003@users.noreply.github.com>
2024-08-13 12:59:19 +0530

    Update State.js

--- src/components/State.js
+++ src/components/State.js
@@ -4,6 +4,7 @@ function State() {
     const [username, setUsername] = useState("John");
     const [bgColor, setbgColor] = useState("grey");
     const [mode, setMode] = useState("Light");
+    const [col, setColor] = useState("");
     const handleChange = () => {
         setUsername(username === "John" ? "Doe": "John");
     };
@@ -12,12 +13,22 @@ function State() {
         setbgColor(bgColor === "grey" ? "white": "grey");
         setMode(mode==="Light"? "Dark":"Light");
     };
+
+    const randomColor = () => {
+        var letters = '0123456789ABCDEF';
+        var color = '#';
+        for (var i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+          }
+          setColor(color);
+    };
     return (
-        <div style={{backgroundColor: bgColor, padding: 350}}>
+        <div style={{backgroundColor: col, padding: 350}}>
             <h1>State</h1>
             <h3>{username}</h3>
             <p><button onClick={handleChange}>Change Username</button></p>
-            <button onClick={changeModes}>{mode} Mode</button>
+            {/*<p><button onClick={changeModes}>{mode} Mode</button></p>*/}
+            <p><button onClick={randomColor} style={{backgroundColor: col, borderRadius: 5, padding: 10}}>{col} Color</button></p>
         </div>
     )
 }
